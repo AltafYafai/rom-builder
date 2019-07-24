553e0178e16adef9a1bf04aaccfb00f37f016329
flagersgit <flagersmail@gmail.com>
2019-07-24 19:51:56 -0400

    try to fix java

--- clone.sh
+++ clone.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # write commands if you want to clone some repos
-sudo apt-get install openjdk-8-jdk
+sudo apt install openjdk-8-jdk
+java -version
 mkdir .repo/local_manifests
 curl https://raw.githubusercontent.com/moto-msm8937-common/local_manifests/staging/lineage-15.1/perry-lineage-15.1.xml -o .repo/local_manifests/perry-lineage-15.1.xml > /dev/null 2>&1
 repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all) -q device/motorola/perry device/motorola/msm8937-common vendor/motorola/perry vendor/motorola/msm8937-common kernel/motorola/msm8937 2>&1 >>logwe 2>&1
