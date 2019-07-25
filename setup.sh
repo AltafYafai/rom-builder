3668dbd0b7ebef84f411df90d12b62ecd75574d4
flagersgit <flagersmail@gmail.com>
2019-07-24 21:56:14 -0400

    Adapt for oreo

--- setup.sh
+++ setup.sh
@@ -16,7 +16,14 @@ useradd -m -d /home/ci ci
 useradd -g ci wheel
 sudo cp github-release /usr/bin
 sudo cp telegram /usr/bin
-sudo apt-get install openjdk-8-jdk
-sudo update-alternatives --config java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt update
+echo debconf shared/accepted-oracle-license-v1-1 select true | \
+sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+sudo debconf-set-selections
+sudo apt install oracle-java8-installer
+sudo apt install oracle-java8-set-default
+javac -version
 echo "$PATH"
 sudo -Hu ci bash -c "bash build.sh"
