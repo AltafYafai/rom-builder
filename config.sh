ffbd48b08113006c7a8b72cfd69c630e3beaa180
Altaf Yafai <49188943+AltafYafai@users.noreply.github.com>
2019-07-31 10:53:32 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-GITHUB_USER=LukmanShaikh
-GITHUB_EMAIL=shaikhlukman01@gmail.com
+GITHUB_USER=AltafYafai
+GITHUB_EMAIL=altaf.alu@gmail.com
 
-KBUILD_BUILD_USER="Luqman"
-KBUILD_BUILD_HOST="Unique"
+KBUILD_BUILD_USER="Altaf"
+KBUILD_BUILD_HOST="Noob_tester"
 
 export oem=xiaomi
 export device=land
@@ -14,6 +14,6 @@ manifest_url="https://github.com/crdroidandroid/android"
 export rom_vendor_name="lineage" # This represent the nams used by different rom vendors, Ex - lineage_land-userdebug, lineage is vendor name.
 branch="9.0"
 
-release_repo="LukmanShaikh/releases"
+release_repo="AltafYafai/release"
 
 timezone="Seoul"
